28965c7ee4a426a4b27301e51cad536015e9ef28
trikking <trikker@qq.com>
2016-09-27 18:11:06 +0800

    实现了拿MQ的queue和queue状态

--- python/dataflume_monitor/common.py
+++ python/dataflume_monitor/common.py
@@ -3,32 +3,10 @@
 
 import paramiko
 import traceback
-import ConfigParser
-import logging
-import multiprocessing
-import time,datetime
+from xml.etree import cElementTree as ElementTree
 
 global_ssh_port = 22
 global_timeout = 20
-
-conffile='conf/monitor.cnf'
-
-logger = logging.getLogger('dataflume-monitor-logger')
-logger.setLevel(logging.INFO)
-
-service_fh = logging.FileHandler('log/service.log')
-service_fh.setLevel(logging.INFO)
-
-error_fh = logging.FileHandler('log/error.log')
-error_fh.setLevel(logging.ERROR)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-service_fh.setFormatter(formatter)
-error_fh.setFormatter(formatter)
-
-logger.addHandler(service_fh)
-logger.addHandler(error_fh)
 
 def ssh_outs(ip, cmd, port = global_ssh_port, user = '', password = ''):
 
@@ -74,85 +52,64 @@ def ssh_outs(ip, cmd, port = global_ssh_port, user = '', password = ''):
     return_info = {"status":status, "message":message, "data":data}
     return return_info
 
-def parse_conf(conffile):
-    config = ConfigParser.ConfigParser()
-    config.read(conffile)
+class XmlListConfig(list):
+    def __init__(self, aList):
+        for element in aList:
+            if element:
+                # treat like dict
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    self.append(XmlDictConfig(element))
+                # treat like list
+                elif element[0].tag == element[1].tag:
+                    self.append(XmlListConfig(element))
+            elif element.text:
+                text = element.text.strip()
+                if text:
+                    self.append(text)
 
-    ### parse extractor parameters
-    extractor_default = {}
-    extractor_para_list = []
-    mq_default = {}
-    mq_para_list = []
-    applier_default = {}
-    applier_para_list = []
 
-    sections = config.sections()
+class XmlDictConfig(dict):
+    '''
+    Example usage:
 
-    extractor_default['ssh_user'] = config.get('extractor-default', 'ssh_user')
-    extractor_default['ssh_port'] = config.get('extractor-default', 'ssh_port')
-    extractor_default['app_pattern'] = config.get('extractor-default', 'app_pattern')
+    >>> tree = ElementTree.parse('your_file.xml')
+    >>> root = tree.getroot()
+    >>> xmldict = XmlDictConfig(root)
 
-    applier_default['ssh_user'] = config.get('applier-default', 'ssh_user')
-    applier_default['ssh_port'] = config.get('applier-default', 'ssh_port')
-    applier_default['app_pattern'] = config.get('applier-default', 'app_pattern')
+    Or, if you want to use an XML string:
 
-    for section in sections:
-        
-        extractor = {}
-        if 'extractor-' in section and section != 'extractor-default':
-            extractor['ip'] = config.get(section, 'ip')
-            #extractor['ssh_user'] = config.get(section, 'ssh_user')
-            #extractor['ssh_port'] = config.get(section, 'ssh_port')
-            extractor['app_dir'] = config.get(section, 'app_dir')
-            #extractor['app_pattern'] = config.get(section, 'app_pattern')
-            extractor['ssh_user'] = extractor_default['ssh_user']
-            extractor['ssh_port'] = extractor_default['ssh_port']
-            extractor['app_pattern'] = extractor_default['app_pattern']
-            extractor_para_list.append(extractor)
+    >>> root = ElementTree.XML(xml_string)
+    >>> xmldict = XmlDictConfig(root)
 
-        applier = {}
-        if 'applier-' in section and section != 'applier-default':
-            applier['ip'] = config.get(section, 'ip')
-            #applier['ssh_user'] = config.get(section, 'ssh_user')
-            #applier['ssh_port'] = config.get(section, 'ssh_port')
-            applier['app_dir'] = config.get(section, 'app_dir')
-            #applier['app_pattern'] = config.get(section, 'app_pattern')
-            applier['ssh_user'] = applier_default['ssh_user']
-            applier['ssh_port'] = applier_default['ssh_port']
-            applier['app_pattern'] = applier_default['app_pattern']
-            applier_para_list.append(applier)
-
-    return extractor_para_list, mq_para_list, applier_para_list
-
-def monitor(monitor_type, para = {}):
-    ip = para['ip']
-    ssh_user = para['ssh_user']
-    ssh_port = para['ssh_port']
-    app_dir = para['app_dir']
-    app_pattern = para['app_pattern']
-    if monitor_type == 'mq':
-        app_port = para['app_port']
-    date = datetime.datetime.now()
-    priv_error_log_date = date - datetime.timedelta(days = 10)
-    while True:
-        monitor_cmd = 'ps -ef | grep %s | grep -v grep' % (app_pattern)
-        monitor_result = ssh_outs(ip = ip, port = ssh_port, cmd = monitor_cmd, user = ssh_user)
-        if monitor_type == 'extractor' or monitor_type == 'applier':
-            if monitor_result['status'] == 'failure':
-                logger.error('%s -- %s -- %s is NOT running!!! Please check!!!' % (ip, monitor_type, app_pattern))
-            else:
-                monitor_log_cmd = """ ls --full-time %s/log/error.log | awk '{print $6 " " $7}' | sed 's/\..*$//g' """ % (app_dir)
-                monitor_log_result = ssh_outs(ip = ip, port = ssh_port, cmd = monitor_log_cmd, user = ssh_user)
-                if monitor_log_result['status'] == 'failure':
-                    logger.critical('%s -- %s -- %s is running, monitor error!!!' % (ip, monitor_type, app_pattern))
-                    logger.critical(monitor_log_result['message'])
+    And then use xmldict for what it is... a dict.
+    '''
+    def __init__(self, parent_element):
+        if parent_element.items():
+            self.update(dict(parent_element.items()))
+        for element in parent_element:
+            if element:
+                # treat like dict - we assume that if the first two tags
+                # in a series are different, then they are all different.
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    aDict = XmlDictConfig(element)
+                # treat like list - we assume that if the first two tags
+                # in a series are the same, then the rest are the same.
                 else:
-                    error_log_date_str = monitor_log_result["data"].strip()
-                    error_log_date = datetime.datetime.strptime(error_log_date_str, "%Y-%m-%d %H:%M:%S")
-                    if priv_error_log_date >= error_log_date:
-                        logger.info('%s -- %s -- %s is running, app log is OK!' % (ip, monitor_type, app_pattern))
-                    else:
-                        logger.error('%s -- %s -- %s new error log found since %s' % 
-                                 (ip, monitor_type, app_pattern, priv_error_log_date.strftime("%Y-%m-%d %H:%M:%S")))
-                    priv_error_log_date = error_log_date
-        time.sleep(5)
+                    # here, we put the list in dictionary; the key is the
+                    # tag name the list elements all share in common, and
+                    # the value is the list itself 
+                    aDict = {element[0].tag: XmlListConfig(element)}
+                # if the tag has attributes, add those to the dict
+                if element.items():
+                    aDict.update(dict(element.items()))
+                self.update({element.tag: aDict})
+            # this assumes that if you've got an attribute in a tag,
+            # you won't be having any text. This may or may not be a 
+            # good idea -- time will tell. It works for the way we are
+            # currently doing XML configuration files...
+            elif element.items():
+                self.update({element.tag: dict(element.items())})
+            # finally, if there are no child tags and no attributes, extract
+            # the text
+            else:
+                self.update({element.tag: element.text})
--- python/dataflume_monitor/monitor.py
+++ python/dataflume_monitor/monitor.py
@@ -6,11 +6,155 @@ import paramiko
 import logging
 import getopt
 import traceback
+import ConfigParser
+import datetime, time
+import urllib2
+from xmltodict import xmltodict
+import json
 from common import *
 
+conffile='conf/monitor.cnf'
+
+logger = logging.getLogger('dataflume-monitor-logger')
+logger.setLevel(logging.INFO)
+
+service_fh = logging.FileHandler('log/service.log')
+service_fh.setLevel(logging.INFO)
+
+error_fh = logging.FileHandler('log/error.log')
+error_fh.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+service_fh.setFormatter(formatter)
+error_fh.setFormatter(formatter)
+
+logger.addHandler(service_fh)
+logger.addHandler(error_fh)
+
+def parse_conf(conffile):
+    config = ConfigParser.ConfigParser()
+    config.read(conffile)
+
+    ### parse extractor parameters
+    extractor_default = {}
+    extractor_para_list = []
+    mq_default = {}
+    mq_para_list = []
+    applier_default = {}
+    applier_para_list = []
+
+    sections = config.sections()
+
+    mq_default['admin_user'] = config.get('mq-default', 'admin_user')
+    mq_default['admin_passwd'] = config.get('mq-default', 'admin_passwd')
+    mq_default['admin_web_port'] = config.get('mq-default', 'admin_web_port')
+
+    extractor_default['ssh_user'] = config.get('extractor-default', 'ssh_user')
+    extractor_default['ssh_port'] = config.get('extractor-default', 'ssh_port')
+    extractor_default['app_pattern'] = config.get('extractor-default', 'app_pattern')
+
+    applier_default['ssh_user'] = config.get('applier-default', 'ssh_user')
+    applier_default['ssh_port'] = config.get('applier-default', 'ssh_port')
+    applier_default['app_pattern'] = config.get('applier-default', 'app_pattern')
+
+    for section in sections:
+
+        mq = {}
+        if 'mq-' in section and section != 'mq-default':
+            mq['ip'] = config.get(section, 'ip')
+            mq['admin_user'] = config.get(section, 'admin_user')
+            mq['admin_passwd'] = config.get(section, 'admin_passwd')
+            mq['admin_web_port'] = config.get(section, 'admin_web_port')
+            mq_para_list.append(mq)
+        
+        extractor = {}
+        if 'extractor-' in section and section != 'extractor-default':
+            extractor['ip'] = config.get(section, 'ip')
+            #extractor['ssh_user'] = config.get(section, 'ssh_user')
+            #extractor['ssh_port'] = config.get(section, 'ssh_port')
+            extractor['app_dir'] = config.get(section, 'app_dir')
+            #extractor['app_pattern'] = config.get(section, 'app_pattern')
+            extractor['ssh_user'] = extractor_default['ssh_user']
+            extractor['ssh_port'] = extractor_default['ssh_port']
+            extractor['app_pattern'] = extractor_default['app_pattern']
+            extractor_para_list.append(extractor)
+
+        applier = {}
+        if 'applier-' in section and section != 'applier-default':
+            applier['ip'] = config.get(section, 'ip')
+            #applier['ssh_user'] = config.get(section, 'ssh_user')
+            #applier['ssh_port'] = config.get(section, 'ssh_port')
+            applier['app_dir'] = config.get(section, 'app_dir')
+            #applier['app_pattern'] = config.get(section, 'app_pattern')
+            applier['ssh_user'] = applier_default['ssh_user']
+            applier['ssh_port'] = applier_default['ssh_port']
+            applier['app_pattern'] = applier_default['app_pattern']
+            applier_para_list.append(applier)
+
+    return mq_para_list, extractor_para_list, applier_para_list
+
+def monitor(monitor_type, para = {}):
+    date = datetime.datetime.now()
+    priv_error_log_date = date - datetime.timedelta(days = 10)
+    while True:
+        if monitor_type == 'mq':
+            ip = para['ip']
+            admin_user = para['admin_user']
+            admin_passwd = para['admin_passwd']
+            admin_web_port = para['admin_web_port']
+            #active_mq_http_baseurl = 'http://%s:%s/api/jolokia' % (ip, admin_web_port)
+
+            #surl = '%s/read/org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName=t_tfr_unload_diffcheck_report/QueueSize' % (active_mq_http_baseurl)
+            #check_status_url = '%s/read/org.apache.activemq:type=Broker,brokerName=localhost,service=Health/CurrentStatus' % (active_mq_http_baseurl)
+            queue_view_url = 'http://%s:%s/admin/xml/queues.jsp' % (ip, admin_web_port)
+
+            passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            passman.add_password(None, queue_view_url, admin_user, admin_passwd)
+            auth_handler = urllib2.HTTPBasicAuthHandler(passman)
+            opener = urllib2.build_opener(auth_handler)
+            urllib2.install_opener(opener)
+            response = urllib2.urlopen(queue_view_url)
+            xml_resContent = response.read()
+            queues_json = xmltodict.parse(xml_resContent)
+            for queue in queues_json["queues"]["queue"]:
+                logger.info("queue名称:", queue["@name"], "剩余未消费数量:", queue["stats"]["@size"])
+
+        if monitor_type == 'extractor' or monitor_type == 'applier':
+            ip = para['ip']
+            ssh_user = para['ssh_user']
+            ssh_port = para['ssh_port']
+            app_dir = para['app_dir']
+            app_pattern = para['app_pattern']
+            monitor_cmd = 'ps -ef | grep %s | grep -v grep' % (app_pattern)
+            monitor_result = ssh_outs(ip = ip, port = ssh_port, cmd = monitor_cmd, user = ssh_user)
+            if monitor_result['status'] == 'failure':
+                logger.error('%s -- %s -- %s is NOT running!!! Please check!!!' % (ip, monitor_type, app_pattern))
+            else:
+                monitor_log_cmd = """ ls --full-time %s/log/error.log | awk '{print $6 " " $7}' | sed 's/\..*$//g' """ % (app_dir)
+                monitor_log_result = ssh_outs(ip = ip, port = ssh_port, cmd = monitor_log_cmd, user = ssh_user)
+                if monitor_log_result['status'] == 'failure':
+                    logger.critical('%s -- %s -- %s is running, monitor error!!!' % (ip, monitor_type, app_pattern))
+                    logger.critical(monitor_log_result['message'])
+                else:
+                    error_log_date_str = monitor_log_result["data"].strip()
+                    error_log_date = datetime.datetime.strptime(error_log_date_str, "%Y-%m-%d %H:%M:%S")
+                    if priv_error_log_date >= error_log_date:
+                        logger.info('%s -- %s -- %s is running, app log is OK!' % (ip, monitor_type, app_pattern))
+                    else:
+                        logger.error('%s -- %s -- %s new error log found since %s' % 
+                                 (ip, monitor_type, app_pattern, priv_error_log_date.strftime("%Y-%m-%d %H:%M:%S")))
+                    priv_error_log_date = error_log_date
+
+        time.sleep(5)
+
 def multi_monitor():
-    (extractor_para_list, mq_para_list, applier_para_list) = parse_conf(conffile)
+    (mq_para_list, extractor_para_list, applier_para_list) = parse_conf(conffile)
     threads = []
+    for mq_para in mq_para_list:
+        t = threading.Thread(target = monitor, args = ('mq', mq_para,))
+        t.daemon = True
+        threads.append(t)
     for extractor_para in extractor_para_list:
         t = threading.Thread(target = monitor, args = ('extractor', extractor_para,))
         t.daemon = True
